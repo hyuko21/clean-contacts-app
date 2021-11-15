2e59cfb1cfb383a01118189a90296bf276f4702f
Victor Ribeiro <hyukodev@gmail.com>
2021-11-14 21:43:49 -0300

    feat(server): add FileSystemContacts repo add() method

--- server/src/infra/db/file-system/fs-contacts.repository.ts
+++ server/src/infra/db/file-system/fs-contacts.repository.ts
@@ -1,8 +1,8 @@
-import { ICheckContactByEmailRepository } from '@/data/protocols/db'
+import { IAddContactRepository, ICheckContactByEmailRepository } from '@/data/protocols/db'
 import { FileSystemAbstractRepository } from '@/common/db/file-system'
 import { ContactModel } from '@/domain/models'
 
-export class FileSystemContactsRepository extends FileSystemAbstractRepository<ContactModel> implements ICheckContactByEmailRepository {
+export class FileSystemContactsRepository extends FileSystemAbstractRepository<ContactModel> implements ICheckContactByEmailRepository, IAddContactRepository {
   constructor () {
     super({ filename: FileSystemContactsRepository.filename })
   }
@@ -11,6 +11,16 @@ export class FileSystemContactsRepository extends FileSystemAbstractRepository<C
     const customers = await this.repository.find()
     const exists = customers.some((customer: any) => customer.email === params.email)
     return exists
+  }
+
+  async add (params: IAddContactRepository.Params): Promise<IAddContactRepository.Result> {
+    await this.repository.add({
+      name: params.name,
+      email: params.email,
+      phone: params.phone,
+      address: params.address
+    })
+    return true
   }
 }
 
