a82a0d0c6657f9b0ad67898d27765ac8a67c878a
Victor Ribeiro <hyukodev@gmail.com>
2021-11-15 09:33:35 -0300

    feat(server): add body parser middleware

--- server/src/main/config/app.ts
+++ server/src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express'
+import setupMiddlewares from './middlewares'
 
 export function buildApp (): Express {
   const app = express()
+  setupMiddlewares(app)
   return app
 }
