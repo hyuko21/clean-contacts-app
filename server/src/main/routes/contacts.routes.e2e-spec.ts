fc4f9f429ae095a224be0aee4f42d64f880875c3
Victor Ribeiro <hyukodev@gmail.com>
2021-11-15 10:46:58 -0300

    test(server): add test cases for validation of POST /contacts

--- server/src/main/routes/contacts.routes.e2e-spec.ts
+++ server/src/main/routes/contacts.routes.e2e-spec.ts
@@ -1,3 +1,5 @@
+
+import faker from 'faker'
 import request from 'supertest'
 import * as testDb from '@/test-helpers/db'
 import * as testApp from '@/test-helpers/app'
@@ -39,6 +41,104 @@ describe('Contacts Routes', () => {
     it('should return 200 with `success` as false if contact already exists', async () => {
       await mockAddOneContact(requestBody)
       await requestTest.expect(200, { success: false, error: 'Contact is invalid' })
+    })
+
+    describe('when contact is invalid should return 200 with `success` as false', () => {
+      it('if contact "name" is missing', async () => {
+        requestBody.name = undefined
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"name" is required' })
+      })
+
+      it('if contact "name" is empty', async () => {
+        requestBody.name = ''
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"name" is not allowed to be empty' })
+      })
+
+      it('if contact "email" is missing', async () => {
+        requestBody.email = undefined
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"email" is required' })
+      })
+
+      it('if contact "email" is empty', async () => {
+        requestBody.email = ''
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"email" is not allowed to be empty' })
+      })
+
+      it('if contact "email" is invalid', async () => {
+        requestBody.email = faker.random.word()
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"email" must be a valid email' })
+      })
+
+      it('if contact "phone" is missing', async () => {
+        requestBody.phone = undefined
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"phone" is required' })
+      })
+
+      it('if contact "phone" is empty', async () => {
+        requestBody.phone = ''
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"phone" is not allowed to be empty' })
+      })
+
+      it('if contact "address.houseNumber" is missing', async () => {
+        requestBody.address.houseNumber = undefined
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.houseNumber" is required' })
+      })
+
+      it('if contact "address.houseNumber" is invalid', async () => {
+        requestBody.address.houseNumber = ''
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.houseNumber" must be a number' })
+      })
+
+      it('if contact "address.streetName" is missing', async () => {
+        requestBody.address.streetName = undefined
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.streetName" is required' })
+      })
+
+      it('if contact "address.streetName" is empty', async () => {
+        requestBody.address.streetName = ''
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.streetName" is not allowed to be empty' })
+      })
+
+      it('if contact "address.city" is missing', async () => {
+        requestBody.address.city = undefined
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.city" is required' })
+      })
+
+      it('if contact "address.city" is empty', async () => {
+        requestBody.address.city = ''
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.city" is not allowed to be empty' })
+      })
+
+      it('if contact "address.state" is missing', async () => {
+        requestBody.address.state = undefined
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.state" is required' })
+      })
+
+      it('if contact "address.state" is empty', async () => {
+        requestBody.address.state = ''
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"address.state" is not allowed to be empty' })
+      })
+
+      it('if contact has unknown property', async () => {
+        requestBody.unknown = faker.random.word()
+        await requestTest.send(requestBody)
+          .expect(200, { success: false, error: '"unknown" is not allowed' })
+      })
     })
 
     it('should return 200 with `success` as true', async () => {
--- server/src/test-helpers/app.ts
+++ server/src/test-helpers/app.ts
@@ -1,15 +1,22 @@
-import setupMiddlewares from '@/main/config/middlewares'
+import { setupMiddlewares, setupHandlers } from '@/main/config/middlewares'
 import express from 'express'
+
+type AppMockOpts = {
+  withValidation: boolean
+}
 
 type AppMock = {
   app: express.Express
   router: express.Router
 }
 
-export const mockApp = (): AppMock => {
+export const mockApp = (opts: AppMockOpts = { withValidation: true }): AppMock => {
   const app = express()
   setupMiddlewares(app)
   const router = express.Router()
   app.use(router)
+  if (opts.withValidation) {
+    setupHandlers(app)
+  }
   return { app, router }
 }
