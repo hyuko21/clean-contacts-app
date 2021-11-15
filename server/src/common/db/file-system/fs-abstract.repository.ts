999e4ff47bff6ebbf82294dae2c6471e61972e58
Victor Ribeiro <hyukodev@gmail.com>
2021-11-15 09:24:04 -0300

    refactor(server): update FsAbstract repo to singleton

--- server/src/common/db/file-system/fs-abstract.repository.ts
+++ server/src/common/db/file-system/fs-abstract.repository.ts
@@ -7,7 +7,7 @@ export abstract class FileSystemAbstractRepository<T extends AbstractModel> impl
   readonly repository: IAbstractRepository.Repository<T>
 
   constructor (private readonly config: FileSystemAbstractRepository.Config) {
-    this.repository = makeFileSystemAbstractRepository(this.config)
+    this.repository = FileSystemAbstractRepositoryFactory.getInstance(this.config)
   }
 
   async connect (): Promise<boolean> {
@@ -15,36 +15,51 @@ export abstract class FileSystemAbstractRepository<T extends AbstractModel> impl
   }
 }
 
-const makeFileSystemAbstractRepository = <T extends AbstractModel> (config: FileSystemAbstractRepository.Config): IAbstractRepository.Repository<T> => {
-  return {
-    async find (): Promise<T[]> {
-      try {
-        const stringified = await FileSystemHelper.getInstance().loadContents(config.filename)
-        const data = JSON.parse(stringified)
-        return data
-      } catch (error) {
-        return []
-      }
-    },
-    async findById (id: string): Promise<T | undefined> {
-      const storedData = await this.find()
-      const item = storedData.find((item) => item.id === id)
-      return item
-    },
-    async add (item: Omit<T, 'id'>): Promise<T> {
-      const newItem = { ...item, id: cuid() } as T
-      const storedData = await this.find()
-      const newData = [...storedData, newItem]
-      await this.replace(newData)
-      return newItem
-    },
-    async replace (data: T[]): Promise<boolean> {
-      const content = JSON.stringify(data)
-      return FileSystemHelper.getInstance().addContents(config.filename, content)
-    },
-    async delete (): Promise<boolean> {
-      return this.replace([] as T[])
+class FileSystemAbstractRepositoryFactory<T extends AbstractModel> implements IAbstractRepository.Repository<T> {
+  private static instance: FileSystemAbstractRepositoryFactory<any>
+  private constructor (private readonly config: FileSystemAbstractRepository.Config) {}
+
+  static getInstance (config: FileSystemAbstractRepository.Config): FileSystemAbstractRepositoryFactory<any> {
+    if (
+      !FileSystemAbstractRepositoryFactory.instance ||
+      config.filename !== FileSystemAbstractRepositoryFactory.instance.config.filename
+    ) {
+      FileSystemAbstractRepositoryFactory.instance = new FileSystemAbstractRepositoryFactory(config)
     }
+    return FileSystemAbstractRepositoryFactory.instance
+  }
+
+  async find (): Promise<T[]> {
+    try {
+      const stringified = await FileSystemHelper.getInstance().loadContents(this.config.filename)
+      const data = JSON.parse(stringified)
+      return data
+    } catch (error) {
+      return []
+    }
+  }
+
+  async findById (id: string): Promise<T | undefined> {
+    const storedData = await this.find()
+    const item = storedData.find((item) => item.id === id)
+    return item
+  }
+
+  async add (item: Omit<T, 'id'>): Promise<T> {
+    const newItem = { ...item, id: cuid() } as T
+    const storedData = await this.find()
+    const newData = [...storedData, newItem]
+    await this.replace(newData)
+    return newItem
+  }
+
+  async replace (data: T[]): Promise<boolean> {
+    const content = JSON.stringify(data)
+    return FileSystemHelper.getInstance().addContents(this.config.filename, content)
+  }
+
+  async delete (): Promise<boolean> {
+    return this.replace([])
   }
 }
 
