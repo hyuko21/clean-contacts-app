0ea8015f1a925dc3c0b0a3b61334fc97d0bb383d
Victor Ribeiro <hyukodev@gmail.com>
2021-11-15 11:01:29 -0300

    chore(server): remove calling dotenv.config() as unnecessary

--- server/src/main/config/env.ts
+++ server/src/main/config/env.ts
@@ -1,8 +1,5 @@
-import dotenv from 'dotenv-flow'
 import { resolve } from 'path'
 import { path as rootPath } from 'app-root-path'
-
-dotenv.config()
 
 export default {
   STORAGE_BASE_PATH: resolve(rootPath, process.env.STORAGE_NAME ?? 'storage'),
