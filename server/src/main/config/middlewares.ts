0b0bd9b1760ec9c40cf34d0adc727c73cf91a6c3
Victor Ribeiro <hyukodev@gmail.com>
2021-11-16 11:47:41 -0300

    feat(server): add cors and content-type middlewares

--- server/src/main/config/middlewares.ts
+++ server/src/main/config/middlewares.ts
@@ -1,8 +1,16 @@
 import { Express } from 'express'
-import { bodyParser, loggerHandler, errorHandler } from '@/main/middlewares'
+import {
+  bodyParser,
+  cors,
+  loggerHandler,
+  errorHandler,
+  contentType
+} from '@/main/middlewares'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
+  app.use(contentType)
   app.use(loggerHandler)
 }
 
--- server/src/main/middlewares/index.ts
+++ server/src/main/middlewares/index.ts
@@ -1,3 +1,5 @@
 export * from './body-parser.middleware'
 export * from './error-handler.middleware'
 export * from './logger.middleware'
+export * from './cors.middleware'
+export * from './content-type.middleware'
