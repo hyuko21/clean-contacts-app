cd1dcebcd0bf39417c649481602c516f0d464308
Victor Ribeiro <hyukodev@gmail.com>
2021-11-16 11:48:01 -0300

    chore(client): add default value for country select component

--- client/src/helpers/CountrySelect.tsx
+++ client/src/helpers/CountrySelect.tsx
@@ -2,7 +2,7 @@ import { countryList } from '../data/country-list'
 
 export const CountrySelect = (props: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>) => (
   <select {...props}>
-    <option selected>{'<Select an option>'}</option>
+    <option selected value=''>{'<Select an option>'}</option>
     {countryList.map(({ value, label }) => <option key={value} value={value}>{label}</option>)}
   </select>
 )
